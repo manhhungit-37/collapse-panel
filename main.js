19c6c01b6253142f5319ca14b764e86cbba30f8d
Tuan Anh <manhhungit.37@gmail.com>
2021-08-02 20:47:45 +0700

    New

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 const heading = document.getElementsByClassName('heading');
-const text = document.getElementsByClassName('text');
 const fas = document.getElementsByClassName('fas');
-
-
-const displayList = Array(heading.length);
+const text = document.getElementsByClassName("text");
 
 for (let i = 0; i < heading.length; i++) {
   heading[i].onclick = () => {
-    if (!displayList[i]) {
-      text[i].classList.remove("text-none");
-      text[i].classList.add("text-block");
-      fas[i].classList.remove("fa-chevron-right");
-      fas[i].classList.add("fa-chevron-down");
-      displayList[i] = true;
+    if (!text[i].style.maxHeight) {
+      text[i].style.maxHeight = text[i].scrollHeight + "px";
+      fas[i].classList.add("rotate-90");
     }
     else {
-      text[i].classList.remove("text-block");
-      text[i].classList.add("text-none");
-      fas[i].classList.remove("fa-chevron-down");
-      fas[i].classList.add("fa-chevron-right");
-      displayList[i] = false;
+      text[i].style.maxHeight = null;
+      fas[i].classList.remove("rotate-90");
     }
   }
 }
-
-console.log(text);
-console.log(displayList);
